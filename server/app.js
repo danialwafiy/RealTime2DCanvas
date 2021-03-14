96b59af0855be7d26b5a1e84e5ff3f9652ee6d7f
danialwafiy <danialwafiy1014@gmail.com>
2021-03-14 14:42:07 +0800

    first commit

---
+++ server/app.js
@@ -0,0 +1,41 @@
+const express = require("express")();
+const http = require("http").Server(express);
+const socketio = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:8080",
+    credentials: true,
+  },
+});
+
+var position = {
+  x: 200,
+  y: 200,
+};
+
+http.listen(3000, () => {
+  console.log("Listening at port 3000");
+});
+
+socketio.on("connection", (socket) => {
+  socket.emit("position", position);
+  socket.on("move", (data) => {
+    switch (data) {
+      case "left":
+        position.x -= 5;
+        socketio.emit("position", position);
+        break;
+      case "right":
+        position.x += 5;
+        socketio.emit("position", position);
+        break;
+      case "up":
+        position.y -= 5;
+        socketio.emit("position", position);
+        break;
+      case "down":
+        position.y += 5;
+        socketio.emit("position", position);
+        break;
+    }
+  });
+});
